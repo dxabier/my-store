10bd92070fd068ee20d1391ca187deb6b2750cb3
dxabier <denispicn@gmail.com>
2023-02-27 20:54:53 -0600

    Iniciación de servidor

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('my-app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server en express");
+});
+
+app.listen(port, () => {
+  console.log("Escuchando en puerto " + port );
+});
+
