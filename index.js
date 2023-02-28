1dd1c253edc42fbae007e3b4534bc47ee2f38f88
dxabier <denispicn@gmail.com>
2023-02-27 23:44:07 -0600

    Utilización de faker para generar productos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -11,13 +12,24 @@ app.get("/nueva-ruta", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  res.json(
-    [
-      {name: "Product 1", price: 100},
-      {name: "Product 2", price: 200}
-    ]
-  );
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl()
+    });
+  }
+
+  res.json(products);
 });
+
+app.get("/products/filter", (req, res) => {
+  res.send("Filter");
+})
 
 app.get("/products/:id", (req, res) => {
   const {id} = req.params
@@ -35,7 +47,27 @@ app.get("/people/:id",(req, res)=> {
     Name: "Fulano",
     orden: 23
   })
-})
+});
+
+app.get("/users", (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send("No hay consultas")
+  }
+});
+
+app.get("/user/:id", (req, res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+
+  })
+});
 
 app.get("/category/:id", (req, res) => {
   const {id} = req.params;
