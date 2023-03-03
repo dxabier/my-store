cb571ce35f4921496f035f5140840416c147be8e
dxabier <denispicn@gmail.com>
2023-03-02 22:39:48 -0600

    Manejo de errores con Boom

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -20,6 +20,7 @@ routerApi(app)
 
 //midlewares deben ir depues del routing, tener en cuenta el orden en que se ejecuta
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
